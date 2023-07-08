763f1066b88c1938fe962a19e8a6677595e67e22
Sam May <smay1@uw.edu>
2023-07-08 17:42:19 -0400

    Update
    Intermediate updates to scripts during review process

--- Scripts/Fine-Scale_Homing_2014-2016_crosses.R
+++ Scripts/Fine-Scale_Homing_2014-2016_crosses.R
@@ -13,15 +13,16 @@ library(MuMIn)
 library(lme4)
 library(sjPlot)
 library(viridis)
+library(scales)
 
 ###JUST THE PO CROSS DATA
 
-PWS_cross_data<-read.csv("../Data/all_streams_parents_paired_14_16_cross.csv")
+PWS_cross_data<-read.csv("../Data/all_streams_parents_paired_14_16_cross.csv") %>% filter(stream_off!="Paddy")
 colorz<-viridis::plasma(5,end = 0.8)
 
 PWS_cross_data %>% group_by(stream_off) %>% 
-  summarize(n=n(),non_na_sire = sum(!is.na(parent_id_sire)),non_na_dam = sum(!is.na(parent_id_dam))) %>% 
-  ungroup() %>% summarize(sum(non_na_sire))
+  dplyr::summarize(n=n(),non_na_sire = sum(!is.na(parent_id_sire)),non_na_dam = sum(!is.na(parent_id_dam))) %>% 
+  ungroup() %>% dplyr::summarize(sum(non_na_sire))
 
 
 model_data<-PWS_cross_data %>% filter(origin_off=="Natural") %>% 
@@ -32,8 +33,32 @@ model_data<-PWS_cross_data %>% filter(origin_off=="Natural") %>%
   drop_na(distance_mouth_dam,distance_mouth_sire,stream_off)
 
 model_data %>% group_by(stream_off) %>% 
-  summarize(n=n(),non_na_sire = sum(!is.na(parent_id_sire)),non_na_dam = sum(!is.na(parent_id_dam))) %>% 
-  ungroup() %>% summarize(sum(non_na_sire))
+  dplyr::summarize(n=n(),non_na_sire = sum(!is.na(parent_id_sire)),non_na_dam = sum(!is.na(parent_id_dam))) %>% 
+  ungroup() %>% dplyr::summarize(sum(non_na_sire))
+
+
+##Boxplots:
+#Scale of Mate Correlation Boxplots:
+
+dam_sire_boxplots <- model_data %>% mutate(mate_distance = distance_mouth_dam - distance_mouth_sire) %>%  group_by(stream_off) %>% 
+  ggplot(aes(x = 1, y = mate_distance, fill = stream_off))+
+  geom_hline(yintercept = 0, lty = 2)+
+  geom_boxplot(aes(color= stream_off),alpha= 0.6,outlier.shape = NA, show.legend = F)+
+  geom_jitter(aes(color= stream_off),alpha = 0.05, size = 0.75, show.legend = F,height = 0)+
+  scale_fill_discrete(type = colorz)+
+  scale_color_discrete(type = colorz)+
+  scale_y_continuous(breaks = c(-2000,-1500,-1000,-500,-100,100,500,1000,1500,2000)) + 
+  theme_classic()+
+  labs(y = "Distance Between Mates (river meter)\n(Dam - Sire)", x = "Stream", color = "Stream", fill = "Stream")+
+  facet_grid(stream_off~.)+
+  theme(axis.title.x = element_blank(),axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
+
+model_data %>% mutate(mate_distance = distance_mouth_dam - distance_mouth_sire) %>% group_by(stream_off) %>% 
+  dplyr::summarise(mean = mean(mate_distance), sd = sd(mate_distance), median = median(mate_distance))
+
+model_data %>% mutate(mate_distance = distance_mouth_dam - distance_mouth_sire) %>% group_by(sex_off) %>% 
+  dplyr::summarise(mean = mean(mate_distance), sd = sd(mate_distance), median = median(mate_distance))
 
 
 DS_distance_global<-lmer(distance_mouth_sire~distance_mouth_dam+(1|stream_off),
@@ -45,14 +70,14 @@ DS_AIC[,2:5]<-round(DS_AIC[,2:5],2)
 names(DS_AIC[1])<-"K"
 DS_AIC$Model<-rownames(DS_AIC)
 for(i in 1:nrow(DS_AIC)) DS_AIC$Model[i]<- as.character(formula(get.models(DS_distance_dredge,subset = T)[[i]]))[3]
-write.csv(DS_AIC,"../Figures/Dam_Sire_AIC_table.csv")
+#write.csv(DS_AIC,"../Figures/Dam_Sire_AIC_table.csv")
 
 DS_distance_best<-DS_distance_global
 summary(DS_distance_best)
 
 DS_distance_pred<-plot_model(DS_distance_best,type="pred")
 
-PWS_cross_data %>% ggplot(aes(y=distance_mouth_sire,x=distance_mouth_dam))+
+dam_sire_glm<-PWS_cross_data %>% ggplot(aes(y=distance_mouth_sire,x=distance_mouth_dam))+
   geom_jitter(aes(color=stream_off),alpha=0.3,height=10,width=10,size=0.8)+
   geom_line(data=data.frame(distance_mouth_dam=c(0,1100),distance_mouth_sire=c(0,1100)),color="red",lty=2,lwd=0.8)+
   guides(colour = guide_legend(override.aes = list(alpha=1)))+
@@ -60,11 +85,127 @@ PWS_cross_data %>% ggplot(aes(y=distance_mouth_sire,x=distance_mouth_dam))+
               aes(x=x,ymin=conf.low,ymax=conf.high),inherit.aes=F,alpha=0.3)+
   geom_line(data=DS_distance_pred$distance_mouth_dam$data,aes(x=x,y=predicted))+
   theme_classic()+
+  theme(legend.position = "none")+
+  labs(y="Inferred Spawning Location of Sires\n(river meter)",
+       x="Inferred Spawning Location of Dams\n(river meter)",
+       color = "Stream")+
+  scale_color_discrete(type=colorz)#+
+  #guides(colour = guide_legend(override.aes = list(alpha=1,size=2)))
+
+ggpubr::ggarrange(dam_sire_glm,dam_sire_boxplots,widths=c(2.5,1),align = "v")
+
+
+####Stream-specific slopes
+DS_distance_global<-lm(distance_mouth_sire~distance_mouth_dam * stream_off,
+                         data=model_data,na.action="na.fail")
+DS_distance_dredge<-dredge(DS_distance_global)
+
+DS_AIC<-as.data.frame(DS_distance_dredge)[5:9]
+DS_AIC[,2:5]<-round(DS_AIC[,2:5],2)
+names(DS_AIC[1])<-"K"
+DS_AIC$Model<-rownames(DS_AIC)
+for(i in 1:nrow(DS_AIC)) DS_AIC$Model[i]<- as.character(formula(get.models(DS_distance_dredge,subset = T)[[i]]))[3]
+#write.csv(DS_AIC,"../Figures/Dam_Sire_AIC_table.csv")
+
+DS_distance_best<-DS_distance_global
+summary(DS_distance_best)
+
+as.data.frame(model_data %>% group_by(stream_off) %>% dplyr::summarise(max = max(distance_mouth_dam)))
+DS_distance_pred<-as.data.frame(model_data %>% group_by(stream_off) %>% dplyr::summarise(distance_mouth_dam = seq(min(distance_mouth_sire),max(distance_mouth_sire),length.out=1000)))
+DS_distance_pred$distance_mouth_sire <- predict(DS_distance_best,newdata = DS_distance_pred)
+DS_distance_pred$se.fit <- predict(DS_distance_best,newdata = DS_distance_pred,se.fit = T)$se.fit
+
+
+dam_sire_glm<-PWS_cross_data %>% ggplot(aes(y=distance_mouth_sire,x=distance_mouth_dam))+
+  geom_jitter(aes(color=stream_off),alpha=0.3,height=10,width=10,size=0.8)+
+  #geom_line(data=data.frame(distance_mouth_dam=c(0,1100),distance_mouth_sire=c(0,1100)),lty=2,lwd=0.5)+
+  guides(colour = guide_legend(override.aes = list(alpha=1)))+
+  geom_ribbon(data=DS_distance_pred,
+              aes(x=distance_mouth_dam,ymin=distance_mouth_sire-se.fit,ymax=distance_mouth_sire+se.fit,
+                  fill=stream_off,color=stream_off),inherit.aes=F,alpha=0.3)+
+  geom_line(data=DS_distance_pred,aes(color=stream_off))+
+  theme_classic()+
+  theme(legend.position = "none")+
   labs(y="Inferred Spawning Location of Sires\n(river meter)",
        x="Inferred Spawning Location of Dams\n(river meter)",
        color = "Stream")+
   scale_color_discrete(type=colorz)+
-  guides(colour = guide_legend(override.aes = list(alpha=1,size=2)))
+  scale_fill_discrete(type=colorz)+
+guides(colour = guide_legend(override.aes = list(alpha=1,size=2)))+
+  facet_grid(stream_off~.)+
+  theme(strip.background = element_blank(),strip.text=element_blank())
+dam_sire_glm
 
 
+
+
+dam_sire_boxplots <- model_data %>% mutate(mate_distance = distance_mouth_dam - distance_mouth_sire) %>%  group_by(stream_off) %>% 
+  ggplot(aes(x = 1, y = mate_distance, fill = stream_off))+
+  geom_hline(yintercept = 0, lty = 2)+
+  geom_boxplot(aes(color= stream_off),alpha= 0.6,outlier.shape = NA, show.legend = F)+
+  geom_jitter(aes(color= stream_off),alpha = 0.05, size = 0.75, show.legend = F,height = 0)+
+  scale_fill_discrete(type = colorz)+
+  scale_color_discrete(type = colorz)+
+  scale_y_continuous(breaks = c(-2000,-1500,-1000,-500,-100,100,500,1000,1500,2000)) + 
+  theme_classic()+
+  labs(y = "Distance Between Mates (river meter)\n(Dam - Sire)", x = "Stream", color = "Stream", fill = "Stream")+
+  facet_grid(stream_off~.)+
+  theme(axis.text.x = element_blank(),axis.title.x = element_blank(),
+        axis.ticks.x = element_blank())
+
+
+ggpubr::ggarrange(dam_sire_glm,dam_sire_boxplots,widths=c(2,1),align = "h")
+
+
+
+
+
+###horizontal:
+mysqrt_trans <- function() {
+  trans_new("mysqrt", 
+            transform = base::sqrt,
+            inverse = function(x) ifelse(x<0, 0, x^2),
+            domain = c(0, Inf))
+}
+
+dam_sire_glm<-model_data %>% ggplot(aes(y=distance_mouth_sire,x=distance_mouth_dam))+
+  geom_jitter(aes(color=stream_off),alpha=0.15,height=10,width=10,size=0.8)+
+  geom_abline(slope=1,lty = 2)+
+  guides(colour = guide_legend(override.aes = list(alpha=1)))+
+  geom_ribbon(data=DS_distance_pred,
+              aes(x=distance_mouth_dam,ymin=distance_mouth_sire-se.fit,ymax=distance_mouth_sire+se.fit,
+                  fill=stream_off,color=stream_off),inherit.aes=F,alpha=0.3)+
+  geom_line(data=DS_distance_pred,aes(color=stream_off))+
+  theme_classic()+
+  theme(legend.position = "none")+
+  labs(y="Inferred Spawning Location\nof Sires (river meter)",
+       x="Inferred Spawning Location of Dams (river meter)",
+       color = "Stream")+
+  scale_color_discrete(type=colorz)+
+  scale_fill_discrete(type=colorz)+
+  guides(colour = guide_legend(override.aes = list(alpha=1,size=2)))+
+  facet_grid(.~stream_off)+
+  theme(strip.background = element_blank(),strip.text=element_blank())+
+  scale_x_continuous(breaks = c(0,300,600,900))+
+  expand_limits(x = 0)
+dam_sire_glm
+
+
+
+dam_sire_boxplots <- model_data %>% mutate(mate_distance = distance_mouth_dam - distance_mouth_sire) %>%  group_by(stream_off) %>% 
+  ggplot(aes(x = 1, y = mate_distance, fill = stream_off))+
+  geom_hline(yintercept = 0, lty = 2)+
+  geom_boxplot(aes(color= stream_off),alpha= 0.6,outlier.shape = NA, show.legend = F,width = 0.4)+
+  geom_jitter(aes(color= stream_off),alpha = 0.15, size = 0.8, show.legend = F,height = 0)+
+  scale_fill_discrete(type = colorz)+
+  scale_color_discrete(type = colorz)+
+  scale_y_continuous(breaks = c(-200-100,100,500)) + 
+  theme_classic()+
+  labs(y = "Distance Between\nMates (Dam - Sire)", x = "Stream", color = "Stream", fill = "Stream")+
+  facet_grid(.~stream_off)+
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
+
+
+ggpubr::ggarrange(dam_sire_boxplots,dam_sire_glm,nrow = 2,heights=c(1,1),align = "v")
 
--- Scripts/Fine-Scale_Homing_2014-2016_singles.R
+++ Scripts/Fine-Scale_Homing_2014-2016_singles.R
@@ -3,8 +3,6 @@
 #and is used to examine the correlation between offspring and parent spawning locations
 
 
-setwd("H:/My Drive/PWS_Empirical/Scripts")
-
 library(tidyverse)
 library(glmmTMB)
 #install_version("MuMIn",version = "1.43.17")
@@ -12,21 +10,22 @@ library(MuMIn)
 library(lme4)
 library(sjPlot)
 library(viridis)
+library(ggallin)
 
 ###JUST THE PO single DATA
 
-PWS_single_data<-read.csv("../Data/all_streams_parents_paired_14_16.csv")
+PWS_single_data<-read.csv("../Data/all_streams_parents_paired_14_16.csv") %>% filter(stream_off!="Paddy")
 colorz<-viridis::plasma(5,end = 0.8)
 
 hist(PWS_single_data$distance_mouth_off)
 hist(PWS_single_data$distance_tide_par)
 
 PWS_single_data %>% group_by(year_off,stream_off,offspring_id) %>% 
-  summarize(non_na = sum(!is.na(parent_id))) %>%
-  group_by(year_off,stream_off,non_na) %>% summarize(n=n()) %>% spread(non_na,n) %>% 
+  dplyr::summarize(non_na = sum(!is.na(parent_id))) %>%
+  group_by(year_off,stream_off,non_na) %>% dplyr::summarize(n=n()) %>% spread(non_na,n) %>% 
   mutate(total_offspring = rowSums(across(where(is.numeric)))) %>% 
   rename(one_parent = '1',two_parents = '2') %>% 
-  ungroup() %>% summarize(sum(two_parents))
+  ungroup() %>% dplyr::summarize(sum(two_parents))
 
 model_data<-PWS_single_data %>% filter(origin_off=="Natural") %>% 
   dplyr::select(offspring_id,parent_id,stream_off,sex_off,DOY_off,length_off,distance_mouth_off,
@@ -39,8 +38,8 @@ model_data<-PWS_single_data %>% filter(origin_off=="Natural") %>%
 
 #Need to remove the first group_by above to get this:
 sample_size_table <- model_data %>% group_by(stream_off,sex_off,offspring_id) %>% 
-  summarize(non_na = sum(!is.na(parent_id))) %>%
-  group_by(stream_off,sex_off,non_na) %>% summarize(n=n()) %>% spread(non_na,n) %>% 
+  dplyr::summarize(non_na = sum(!is.na(parent_id))) %>%
+  group_by(stream_off,sex_off,non_na) %>% dplyr::summarize(n=n()) %>% spread(non_na,n) %>% 
   mutate(total_offspring = rowSums(across(where(is.numeric)))) %>% 
   rename(one_parent = '1',two_parents = '2') %>% 
   mutate(total_parents = one_parent + (2*two_parents))#%>% 
@@ -49,10 +48,127 @@ sample_size_table <- model_data %>% group_by(stream_off,sex_off,offspring_id) %>
 
 #Proportion Intertidal
 prop_intertidal <- model_data %>% group_by(stream_off,sex_off,intertidal_off) %>% 
-  summarize(n=n()) %>% mutate(total = sum(n), prop_int = round(n/total,2)) %>% filter(intertidal_off=="Intertidal")
+  dplyr::summarize(n=n()) %>% mutate(total = sum(n), prop_int = round(n/total,2)) %>% filter(intertidal_off=="Intertidal")
 
- #Global distance model:
-global_distance<-lmer(distance_mouth_off~distance_mouth_par
+#Individual distributions:
+# Transform the dataset to a long format
+model_data_long <- model_data %>% 
+  pivot_longer(
+    cols = c(distance_mouth_par, distance_mouth_off),
+    names_to = "Year",
+    values_to = "distance_mouth"
+  ) %>% 
+  mutate(Year = recode(Year, "distance_mouth_par" = "Parents (2014)", "distance_mouth_off" = "Offspring (2016)"))
+
+# Create a single plot with facets
+collections_plot <- model_data_long %>%
+  ggplot(aes(x = stream_off, y = distance_mouth, fill = stream_off)) +
+  geom_jitter(aes(color = stream_off), alpha = 0.3, size = 0.75, show.legend = FALSE, height = 0) +
+  scale_fill_discrete(type = colorz)+
+  scale_color_discrete(type = colorz)+
+  scale_y_continuous(breaks = c(0,100,500,1000,1500,2000)) +
+  theme_classic() +
+  geom_segment(data = high_tide_line, 
+               aes(y = htide,yend = htide,x=c(1,2,3,4,1,2,3,4) - 0.50,xend = c(1,2,3,4,1,2,3,4)+0.50),
+               lty=2, lwd = 1, color="black") +
+  labs(y = "Sampling Locations", x = "Stream", color = "Stream", fill = "Stream") +
+  facet_wrap(~Year)
+
+collections_plot
+#5 x 5.5 ish
+
+library(patchwork)
+
+# Combine the plots
+#combined_plot <- Map.fig / collections_plot
+#combined_plot <- combined_plot + plot_layout(heights = c(2, 1),widths=c(2,1))
+#combined_plot
+
+#6 x 8.5 ish? 
+
+#Scale of Homing Boxplots:
+max_dist <- model_data %>%  group_by(stream_off) %>% dplyr::summarise(max(distance_mouth_par))
+ model_data %>%  group_by(stream_off) %>% dplyr::filter(distance_mouth_par == max(distance_mouth_par))
+
+model_data %>% mutate(homing_distance = distance_mouth_par - distance_mouth_off) %>%  group_by(stream_off) %>% 
+  ggplot(aes(x = stream_off, y = homing_distance, fill = stream_off))+
+  geom_hline(yintercept = 0, lty = 2)+
+  geom_boxplot(aes(color= stream_off),alpha= 0.6,outlier.shape = NA, show.legend = F)+
+  geom_jitter(aes(color= stream_off),alpha = 0.05, size = 0.75, show.legend = F,height = 0)+
+  scale_fill_discrete(type = colorz)+
+  scale_color_discrete(type = colorz)+
+  scale_y_continuous(breaks = c(-2000,-1500,-1000,-500,-100,100,500,1000,1500,2000)) + 
+  theme_classic()+
+  labs(y = "Homing Distance (river meter)\n(Parent - Offspring)", x = "Stream", color = "Stream", fill = "Stream")
+
+model_data %>% mutate(homing_distance = distance_mouth_par - distance_mouth_off) %>%  group_by(stream_off) %>% 
+  dplyr::summarise(mean = mean(homing_distance), sd = sd(homing_distance), median = median(homing_distance))
+
+model_data %>% mutate(homing_distance = distance_mouth_par - distance_mouth_off) %>% group_by(sex_off) %>% 
+  dplyr::summarise(mean = mean(homing_distance), sd = sd(homing_distance), median = median(homing_distance))
+
+#What is the scale of sampling? Taking the mean in each stream on each day of the distance between sampling locations:
+#will need to do this for 2014 and 2016 separately
+sampling_data_2016<-PWS_single_data %>% 
+  dplyr::select(stream_off,DOY_off,distance_mouth_off) %>% drop_na() %>%  
+  distinct() %>% group_by(stream_off,DOY_off)
+mean_int_2016<-sampling_data_2016 %>%
+  group_by(stream_off,DOY_off) %>%
+  arrange(DOY_off,distance_mouth_off,.by_group = T) %>%
+  mutate(sampling_interval = distance_mouth_off - lag(distance_mouth_off, default = first(distance_mouth_off))) %>% 
+  dplyr::summarize(n = n()) %>% mutate(year = 2016) %>% rename(stream = stream_off,DOY = DOY_off)
+sampling_data_2014<-PWS_single_data %>% 
+  dplyr::select(stream_par,DOY_par,distance_mouth_par) %>% drop_na() %>%  
+  distinct() %>% group_by(stream_par,DOY_par)
+mean_int<-sampling_data_2014 %>%
+  group_by(stream_par,DOY_par) %>%
+  arrange(DOY_par,distance_mouth_par,.by_group = T) %>%
+  mutate(sampling_interval = distance_mouth_par - lag(distance_mouth_par, default = first(distance_mouth_par))) %>% 
+  dplyr::summarize(n = n()) %>% mutate(year = 2014) %>% rename(stream = stream_par,DOY = DOY_par) %>% 
+  rbind(mean_int_2016)
+mean_int %>% ggplot(aes(x=n))+
+  geom_histogram(binwidth = 1,aes(fill = stream),position="dodge",color="black")+
+  geom_vline(data = mean_int %>% group_by(stream,year) %>% dplyr::summarize(mean=mean(n)), aes(xintercept = mean),lty = 2)+
+  facet_grid(year~stream)+
+  theme_classic()+
+  labs(x = "Number of sampling\nlocations per day",y = "Count",fill = "Stream")
+
+mean_int_table<-mean_int %>% group_by(stream,year) %>% dplyr::summarize(mean_n_sample_sites=mean(n),sd_n_sample_sites = sd(n))
+
+by_day_sampling_intervals_2016<-sampling_data_2016 %>%
+  group_by(stream_off,DOY_off) %>%
+  arrange(DOY_off,distance_mouth_off,.by_group = T) %>%
+  mutate(sampling_interval = distance_mouth_off - lag(distance_mouth_off, default = first(distance_mouth_off))) %>% 
+  filter(sampling_interval>0) %>% dplyr::summarize(mean_sampling_interval = mean(sampling_interval),
+                                                   var_sampling_interval = var(sampling_interval),
+                                                   sd_sampling_interval = sd(sampling_interval),
+                                                   n_sampling_sites = n()) 
+
+by_creek_sampling_intervals_2016<-by_day_sampling_intervals_2016 %>% group_by(stream_off) %>% 
+  dplyr::summarize(mean_creek_interval = mean(mean_sampling_interval),
+                   sd_creek_interval = sd(mean_sampling_interval)) %>% 
+  rename(stream = stream_off) %>% mutate(year = 2016)
+
+by_day_sampling_intervals_2014<-sampling_data_2014 %>%
+  group_by(stream_par,DOY_par) %>%
+  arrange(DOY_par,distance_mouth_par,.by_group = T) %>%
+  mutate(sampling_interval = distance_mouth_par - lag(distance_mouth_par, default = first(distance_mouth_par))) %>% 
+  filter(sampling_interval>0) %>% dplyr::summarize(mean_sampling_interval = mean(sampling_interval),
+                                                   var_sampling_interval = var(sampling_interval),
+                                                   sd_sampling_interval = sd(sampling_interval),
+                                                   n_sampling_sites = n()) 
+
+by_creek_sampling_intervals_2014<-by_day_sampling_intervals_2014 %>% group_by(stream_par) %>% 
+  dplyr::summarize(mean_creek_interval = mean(mean_sampling_interval),
+                   sd_creek_interval = sd(mean_sampling_interval)) %>% 
+  rename(stream = stream_par) %>% mutate(year = 2014)
+
+rbind(by_creek_sampling_intervals_2014,by_creek_sampling_intervals_2016) %>% arrange(stream) %>% 
+  left_join(mean_int_table)
+
+ 
+#Global distance model:
+global_distance<-lmer(distance_mouth_off~distance_mouth_par +
                      sex_off+DOY_off+length_off+(1|stream_off),
                    data=model_data,na.action=na.fail)
 summary(global_distance)
@@ -75,7 +191,7 @@ for(i in 1:nrow(distance_AIC)) distance_AIC$Model[i]<- as.character(formula(get.
 #Parent-Offspring regression best fit line:
 distance_pred<-plot_model(best_distance,type="pred")
 
-high_tide_line<-PWS_single_data %>% group_by(stream_off) %>% summarize(htide=min(unique(high_tide_off)))
+high_tide_line<-PWS_single_data %>% group_by(stream_off) %>% dplyr::summarize(htide=min(unique(high_tide_off)))
 
 distance_pred$distance_mouth_par$data %>% 
   ggplot(aes(y=predicted,x=x))+
@@ -102,11 +218,127 @@ distance_pred$distance_mouth_par$data %>%
        x="Inferred Spawning Location\nof Parent (river meters)",
        y = "Inferred Spawning Location\nof Offspring (river meters)")
   
+
+
+
+
+####Stream Specific Model:
+
+#Global distance model:
+global_distance<-lm(distance_mouth_off~distance_mouth_par*stream_off +
+                        sex_off+DOY_off+length_off,
+                      data=model_data,na.action=na.fail)
+summary(global_distance)
+
+distance_dredge<-dredge(global_distance)
+
+best_distance <- lm(distance_mouth_off~distance_mouth_par*stream_off+
+                        sex_off+DOY_off,
+                      data=model_data,na.action=na.fail)
+summary(best_distance)
+
+
+distance_AIC<-as.data.frame(distance_dredge)[8:12]
+distance_AIC[,2:5]<-round(distance_AIC[,2:5],2)
+names(distance_AIC[1])<-"K"
+distance_AIC$Model<-rownames(distance_AIC)
+for(i in 1:nrow(distance_AIC)) distance_AIC$Model[i]<- as.character(formula(get.models(distance_dredge,subset = T)[[i]]))[3]
+#write.csv(distance_AIC,"../Figures/distance_AIC_table.csv")
+
+#Parent-Offspring regression best fit line:
+distance_pred<-as.data.frame(model_data %>% group_by(stream_off) %>% 
+                               dplyr::summarise(distance_mouth_par = seq(min(distance_mouth_par),max(distance_mouth_par),length.out=1000),
+                                                DOY_off = seq(min(DOY_off),max(DOY_off),length.out = 1000),
+                                                sex_off = "Male"))
+distance_pred$distance_mouth_off <- predict(best_distance,newdata = distance_pred)
+distance_pred$se.fit <- predict(best_distance,newdata = distance_pred,se.fit = T)$se.fit
+
+high_tide_line<-PWS_single_data %>% group_by(stream_off) %>% dplyr::summarize(htide=min(unique(high_tide_off)))
+
+par_off_glm<-distance_pred %>% 
+  ggplot(aes(y=distance_mouth_off,x=distance_mouth_par,color=stream_off))+
+  #geom_vline(data=high_tide_line,aes(xintercept=htide,color=stream_off),show.legend = F,lty=2,lwd=0.8)+
+  
+  geom_segment(data=high_tide_line,aes(x=htide,y=-100,xend=htide,yend=-20,color=stream_off),
+               lwd=1.2,show.legend = F)+
+  geom_segment(data=high_tide_line,aes(x=-175,xend=-20, y=htide,yend=htide,color=stream_off),
+               lwd=1.2,show.legend = F)+
+  coord_cartesian(ylim = c(-20, 750),xlim=c(-100,1200), clip="on") +
+  
+  #geom_jitter(data=model_data,aes(y=distance_mouth_off,x=distance_mouth_par,
+  #                                color=stream_off),
+  #            pch=16,alpha = 0.3,width = 10,height=10,size=0.8)+
+  guides(colour = guide_legend(override.aes = list(alpha=1,size=2)))+
+  
+  geom_ribbon(aes(ymin=distance_mouth_off-se.fit,ymax=distance_mouth_off+se.fit,color = stream_off,fill=stream_off),alpha=0.3,show.legend = F)+
+  geom_line(lwd=0.8,aes(color=stream_off),show.legend = F)+
+  geom_abline(slope=1,intercept=0,lty=2,lwd=0.5)+
+  scale_color_discrete(type = colorz)+
+  scale_fill_discrete(type = colorz)+
+  theme_classic()+
+  theme(legend.position = "none",strip.background = element_blank(),
+        strip.text = element_blank())+
+  facet_grid(.~stream_off)+
+  labs(color="Stream",
+       x="Inferred Spawning Location of Parent (river meters)",
+       y = "Inferred Spawning Location\nof Offspring (river meters)")
+
+
+par_off_boxplot<-model_data %>% mutate(homing_distance = distance_mouth_par - distance_mouth_off) %>%  group_by(stream_off) %>% 
+  ggplot(aes(x = 1, y = homing_distance, fill = stream_off))+
+  geom_hline(yintercept = 0, lty = 2)+
+  geom_boxplot(aes(color= stream_off),alpha= 0.7,
+               outlier.shape = NA, show.legend = F,width = 0.4)+
+  geom_jitter(aes(color= stream_off),alpha = 0.07, size = 0.8, show.legend = F,height = 0)+
+  scale_fill_discrete(type = colorz)+
+  scale_color_discrete(type = colorz)+
+  theme_classic()+
+  scale_y_continuous(breaks = c(-1000,-500,-100,100,500,1000),limits = c(-1100,1100)) + 
+  theme(axis.title.x = element_blank(),axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())+
+  facet_grid(.~stream_off)+
+  labs(y = "Homing Distance\n(Parent - Offspring)", x = "Stream", color = "Stream", fill = "Stream")
+
+ggarrange(par_off_boxplot,par_off_glm,nrow=2,heights=c(1,1.5),align = "v")
   
 #Intertidal/Upstream Binomial model:
 global_IU<-glmer(intertidal_off~intertidal_par+
                            sex_off+DOY_off+length_off+(1|stream_off),
                          data=model_data,family = "binomial",
+                 na.action=na.fail)
+summary(global_IU)
+
+IU_dredge<-dredge(global_IU)
+
+
+IU_AIC<-as.data.frame(IU_dredge)[6:10]
+IU_AIC[,2:5]<-round(IU_AIC[,2:5],2)
+colnames(IU_AIC)[1]<-"K"
+IU_AIC$Model<-rownames(IU_AIC)
+for(i in 1:nrow(IU_AIC)) IU_AIC$Model[i]<- as.character(formula(get.models(IU_dredge,subset = T)[[i]]))[3]
+#write.csv(IU_AIC,"../Figures/Intertidal_Upstream_AIC_table.csv")
+
+
+best_fit_IU<-glmer(intertidal_off~intertidal_par+
+                     sex_off+DOY_off+(1|stream_off),
+                   data=model_data,family = "binomial",
+                   na.action=na.fail)
+summary(best_fit_IU)
+
+IU_pred<-plot_model(best_fit_IU,type="pred")
+IU_pred$intertidal_par$data$x<-c("Intertidal","Upstream")
+IU_pred$intertidal_par$data %>% 
+  ggplot(aes(y=predicted,x=x))+
+  geom_point()+
+  geom_errorbar(aes(ymin=conf.low,ymax=conf.high),width=0.2)+
+  labs(y="Proportion Spawning Upstream",x="Inferred Natal Rearing Location")+
+  theme_classic()
+
+####Stream-specific:
+#Intertidal/Upstream Binomial model:
+global_IU<-glmer(intertidal_off~intertidal_par+
+                   sex_off+DOY_off+length_off+(1|stream_off),
+                 data=model_data,family = "binomial",
                  na.action=na.fail)
 summary(global_IU)
 
